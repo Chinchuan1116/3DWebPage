7b995177d881e2122277fb4a5e29bc6eb23a70fd
Chinchuan1116 <chinchuan49@gmail.com>
2023-11-03 00:33:44 +0800

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "../pages/Tab";
+
+export {CustomButton, Tab, FilePicker, AIPicker, ColorPicker}
